695a3996f0d40327146e2aaa135811beb61ee814
Paul Christian-Brown <pcbeebusiness@gmail.com>
2025-07-29 04:32:56 -0400

    New setup template with working Tailwind and Astro

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{html,js,astro,ts}'],
+  theme: {
+    extend: {
+    colors: {
+      primary: '#1e40af',
+      secondary: '#facc15',
+    },
+    fontFamily: {
+      display: ['Inter', 'sans-serif'],
+    },
+  },
+  },
+  plugins: [require tailwindcss/typography],
+};
